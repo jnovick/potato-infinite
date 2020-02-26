876dafc2dcac616c21cab6746eb4915fced0023c
Joshua Novick <novickjd@gmail.com>
2020-02-26 17:14:07 -0500

    asdgasdf

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Potato
+          Edit <code>src/App.js</code> and save to reload. Potato 1
         </p>
         <a
           className="App-link"
